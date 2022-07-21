63a04ac3f04eda7e70295940776c4175c1057832
MANISHA <manishapanchal169@gmail.com>
2022-07-21 10:05:52 +0530

    feat: create search comp. and import it in app comp.

---
+++ src/component/Search/Search.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <p>Search Your Favorite Cocktail</p>
+        <form>
+          <input type="search" />
+        </form>
+      </div>
+    );
+  }
+}
+export default Search;
