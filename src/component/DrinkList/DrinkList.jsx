cde4ce176de00ebfd0cc07922df2a18380d3eb77
MANISHA <manishapanchal169@gmail.com>
2022-07-21 11:22:26 +0530

    feat: :sparkles: add dynamic data in drinklist
    using contextApi

--- src/component/DrinkList/DrinkList.jsx
+++ src/component/DrinkList/DrinkList.jsx
@@ -1,17 +1,26 @@
-import React, { Children, Component } from 'react';
-
-class DrinkList extends Component{
-
-
-  render(){
-    return(
-      <>
-      <Drink>{Children}</Drink>
-      <h5>hii I am drinkList</h5>
-      </>
-    )
+import React, { Component } from "react";
+import { DrinkContext } from "../../App";
+import Drink from "../Drink/Drink";
+class DrinkList extends Component {
+  render() {
+    return (
+      <DrinkContext.Consumer>
+        {(arr) => {
+          return arr.map((currItem) => {
+            return (
+              <Drink
+                name={currItem.strDrink}
+                img={currItem.strDrinkThumb}
+                glass={currItem.strGlass}
+                info={currItem.strAlcoholic}
+                key={currItem.idDrink}
+              />
+            );
+          });
+        }}
+      </DrinkContext.Consumer>
+    );
   }
 }
-
 
 export default DrinkList;
