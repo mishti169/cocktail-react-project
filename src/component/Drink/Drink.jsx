b4000d3824dcd4fc48ee7a10b5ad200bf642c884
MANISHA <manishapanchal169@gmail.com>
2022-07-21 10:36:46 +0530

    feat/create drink comp. with styling

--- src/component/Drink/Drink.jsx
+++ src/component/Drink/Drink.jsx
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import "./Drink.css";
 class Drink extends Component {
-
-
-  render(){
-
-
-
-    return(
-      <>
-      <h6>hi i am drink</h6>
-      </>
-    )
+  render() {
+    return (
+      <div className="drink">
+        <img
+          src="https://www.thecocktaildb.com/images/media/drink/1mvjxg1504348579.jpg"
+          alt="drink"
+          width={260}
+        />
+        <div className="drink-details">
+          <span>Casino</span>
+          <span>Cocktail glass</span>
+          <span>Alcoholic</span>
+          <span>DETAILS</span>
+        </div>
+      </div>
+    );
   }
 }
 export default Drink;
