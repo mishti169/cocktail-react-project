77d6bd985cdb2beafd6a3eb0c5650a5572632909
MANISHA <manishapanchal169@gmail.com>
2022-07-21 18:34:02 +0530

    refactor: :recycle: make  getDrinks  function
    which returns Drink comp with dynamic  data

--- src/component/DrinkList/DrinkList.jsx
+++ src/component/DrinkList/DrinkList.jsx
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
 import { DrinkContext } from "../../App";
 import Drink from "../Drink/Drink";
+import "./DrinkList.css";
 class DrinkList extends Component {
+  getDrinks = (arr) => {
+    return arr.map((currItem) => {
+      return (
+        <Drink
+          name={currItem.strDrink}
+          img={currItem.strDrinkThumb}
+          glass={currItem.strGlass}
+          info={currItem.strAlcoholic}
+          key={currItem.idDrink}
+        />
+      );
+    });
+  };
   render() {
     return (
       <DrinkContext.Consumer>
         {(arr) => {
-          return arr.map((currItem) => {
-            return (
-              <Drink
-                name={currItem.strDrink}
-                img={currItem.strDrinkThumb}
-                glass={currItem.strGlass}
-                info={currItem.strAlcoholic}
-                key={currItem.idDrink}
-              />
-            );
-          });
+          return (
+            <div className="drink-list-container">
+              <div className="drink-list">{this.getDrinks(arr)}</div>;
+            </div>
+          );
         }}
       </DrinkContext.Consumer>
     );
